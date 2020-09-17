284cbaf99a66f06f0314c606af5556de713ffc83
Cauê Reple <replecaue@gmail.com>
2020-09-17 14:53:56 +0200

    Introdução ao MaterialApp com Flutter
    - Utilização e aplicação dos Widgets:  MaterialApp, Scaffold, appBar, floatingActionButton.

--- lib/main.dart
+++ lib/main.dart
@@ -2,31 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Column(
-      children: <Widget>[
-        Text(
-          'Deliver features faster',
-          textDirection: TextDirection.ltr,
-        ),
-        Text(
-          'Craft beautiful UIs',
-          textDirection: TextDirection.ltr,
-        ),
-        Column(
-          children: <Widget>[
-            Text(
-              'Craft beautiful UIs',
-              textDirection: TextDirection.ltr,
-            ),
-          ],
-        ),
-        Expanded(
-          child: FittedBox(
-            fit: BoxFit.contain, // otherwise the logo will be tiny
-            child: const FlutterLogo(),
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            ('Transferências'),
           ),
         ),
-      ],
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+        ),
+      ),
     ),
   );
 }
