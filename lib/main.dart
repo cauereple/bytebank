553a6edbd41d2fcf53bb846a6da7934a853ccc39
Cauê Reple <replecaue@gmail.com>
2020-09-17 13:03:14 +0200

    Utilização da Column Class
    - Preparação do ambiente para o Flutter
    
    - Criação e execução do primeiro projeto
    
    - Estrutura de Árvore de Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,32 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Text('Bem vindo ao Bytebank', textDirection: TextDirection.ltr,));
+  runApp(
+    Column(
+      children: <Widget>[
+        Text(
+          'Deliver features faster',
+          textDirection: TextDirection.ltr,
+        ),
+        Text(
+          'Craft beautiful UIs',
+          textDirection: TextDirection.ltr,
+        ),
+        Column(
+          children: <Widget>[
+            Text(
+              'Craft beautiful UIs',
+              textDirection: TextDirection.ltr,
+            ),
+          ],
+        ),
+        Expanded(
+          child: FittedBox(
+            fit: BoxFit.contain, // otherwise the logo will be tiny
+            child: const FlutterLogo(),
+          ),
+        ),
+      ],
+    ),
+  );
 }
-
