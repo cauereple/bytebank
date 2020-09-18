f9613ffd40604dac0fd38d2b1c516359b904376e
Cauê Reple <replecaue@gmail.com>
2020-09-18 11:16:12 +0200

    Refatoração e organização do código criando classes

--- lib/main.dart
+++ lib/main.dart
@@ -4,24 +4,7 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: [
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('100.0'),
-                subtitle: Text('1000'),
-              )
-            ),
-            Card(
-              child: ListTile( 
-                leading: Icon(Icons.monetization_on),
-                title: Text('200.0'),
-                subtitle: Text('1000'),
-              )
-            ),
-          ],
-        ),
+        body: ListaTransferencias(),
         appBar: AppBar(
           title: Text(
             ('Transferências'),
@@ -33,4 +16,42 @@ void main() {
       ),
     ),
   );
+}
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ItemTransferencia(Transferencia(100.0, 1000)),
+        ItemTransferencia(Transferencia(200.0, 2000)),
+        ItemTransferencia(Transferencia(300.0, 3000)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+
+  final Transferencia _transferencia;
+
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+      leading: Icon(Icons.monetization_on),
+      title: Text(_transferencia.valor.toString()),
+      subtitle: Text(_transferencia.numeroConta.toString()),
+    ));
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
 }
