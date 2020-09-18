f16b97e7631ea1db708f21815b89110401761592
Cauê Reple <replecaue@gmail.com>
2020-09-18 09:52:05 +0200

    Começando a criar a lista do app

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,24 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        body: Column(
+          children: [
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('100.0'),
+                subtitle: Text('1000'),
+              )
+            ),
+            Card(
+              child: ListTile( 
+                leading: Icon(Icons.monetization_on),
+                title: Text('200.0'),
+                subtitle: Text('1000'),
+              )
+            ),
+          ],
+        ),
         appBar: AppBar(
           title: Text(
             ('Transferências'),
