eb998a922dd57e0e0e26cf167071e54486f87035
Cauê Reple <replecaue@gmail.com>
2020-09-18 15:46:16 +0200

    Terminando a feature do Formulário de Transferência
    - Configurando os TextField e RaisedButton.
    
    - Aplicando o controller para "pegar" o conteúdo inserido pelo usuário.
    
    - Entendimento de conversão de números para strings com o parse, toString, int.tryParse, souble.tryParse.
    
    - Fazendo uma condição if e novas variáveis.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,11 +18,64 @@ class BytebankApp extends StatelessWidget {
 }
 
 class FormularioTransferencia extends StatelessWidget {
+
+  final TextEditingController _controladorCampoNumeroConta =
+      TextEditingController();
+  final TextEditingController _controladorCampoValor = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('Criando Transferência'),),
-        body: Text('Teste'));
+        appBar: AppBar(
+          title: Text('Criando Transferência'),
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: _controladorCampoNumeroConta,
+                style: TextStyle(
+                  fontSize: 24.0,
+                ),
+                decoration: InputDecoration(
+                  labelText: 'Número da conta',
+                  hintText: '0000',
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: _controladorCampoValor,
+                style: TextStyle(
+                  fontSize: 24.0,
+                ),
+                decoration: InputDecoration(
+                  icon: Icon(Icons.monetization_on),
+                  labelText: 'Valor',
+                  hintText: '0.00',
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            RaisedButton(
+              child: Text('Confirmar'),
+              onPressed: () {
+                debugPrint(_controladorCampoNumeroConta.text);
+                debugPrint(_controladorCampoValor.text);
+                final int numeroConta = int.tryParse(_controladorCampoNumeroConta.text);
+                final double valor = double.tryParse(_controladorCampoValor.text);
+                if(numeroConta != null && valor != null){
+                  final transferenciaCriada = Transferencia(valor, numeroConta);
+                  debugPrint('$transferenciaCriada');
+                }
+
+              },
+            )
+          ],
+        ));
   }
 }
 
@@ -46,9 +101,7 @@ class ListaTransferencias extends StatelessWidget {
 }
 
 class ItemTransferencia extends StatelessWidget {
-
   final Transferencia _transferencia;
-
 
   ItemTransferencia(this._transferencia);
 
@@ -68,4 +121,9 @@ class Transferencia {
   final int numeroConta;
 
   Transferencia(this.valor, this.numeroConta);
+
+  @override
+  String toString() {
+    return 'Transferencia{valor: $valor, numeroConta: $numeroConta}';
+  }
 }
